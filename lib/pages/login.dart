345d6f8359b3f6de9250a6c6fe255486def906ef
Ankit Bhalla <ankit.bhalla@cars24.com>
2021-05-18 02:12:21 +0530

    5th step animation on login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutterdemo/utils/routes.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name = '';
+  bool animated = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +42,38 @@ class Login extends StatelessWidget {
                     )
                   ])),
               SizedBox(height: 40),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  child: Text('Login'),
-                  style: TextButton.styleFrom(minimumSize: Size(120, 40))),
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    animated = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    child: Container(
+                        width: animated ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: animated
+                            ? Icon(Icons.done, color: Colors.white)
+                            : Text('LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold)),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(animated ? 25 : 5)))),
+              )
+              // ElevatedButton(
+              //     onPressed: () {
+              //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+              //     },
+              //     child: Text('Login'),
+              //     style: TextButton.styleFrom(minimumSize: Size(120, 40))),
             ],
           ),
         ));
